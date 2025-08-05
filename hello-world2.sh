234032cacf0ca6901e46d40eaff08f0d99e19c07
Pavan Bokka <rampavan.bokka@gmail.com>
2025-08-05 16:25:49 +0530

    new commands added

--- hello-world2.sh
+++ hello-world2.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "all variables passed to the script: $@"
+echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Current directory: $pwd"
+echo "user running this script: $user"
+echo "home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"
