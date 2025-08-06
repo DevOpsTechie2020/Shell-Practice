511775aa7dc61c06d090db4006abedbfae91da79
Pavan Bokka <rampavan.bokka@gmail.com>
2025-08-06 16:48:21 +0530

    new code

---
+++ Logs.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+mkdir -p $LOGS_FOLDER
+echo "$R Script started executing at: $(date) $N" &>>$LOG_FILE
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error: please run this script with root access $N" &>>$LOG_FILE
+    exit1
+else
+    echo -e "$G You are running with root access $N" &>>$LOG_FILE
+fi     
+VALIDATE(){
+   if [ $1 -eq 0 ]
+    then
+        echo -e "Installing $2 is $G SUCCESS $N." &>>$LOG_FILE
+    else
+        echo -e "Installing $2 is $R FAILURE $N." &>>$LOG_FILE
+        exit 1
+    fi  
+}
+dnf list installed mysql &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo -e "$R MySQL is not installed...initialising installation $N" &>>$LOG_FILE
+    dnf install mysql -y &>>$LOG_FILE
+    VALIDATE $? "MySQL"
+else
+    echo -e "$Y MySQL is already installed. $N" &>>$LOG_FILE
+fi
+dnf list installed python3 &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo -e "$R python3 is not installed...initialising installation $N" &>>$LOG_FILE
+    dnf install python3 -y &>>$LOG_FILE
+    VALIDATE $? "python3"
+else
+    echo -e "$Y python3 is already installed. $N" &>>$LOG_FILE
+fi
+dnf list installed nginx &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo -e "$R Nginx is not installed...initialising installation $N" &>>$LOG_FILE
+    dnf install nginx -y &>>$LOG_FILE
+    VALIDATE $? "nginx"
+else
+    echo -e "$Y nginx is already installed. $N" &>>$LOG_FILE
+fi
