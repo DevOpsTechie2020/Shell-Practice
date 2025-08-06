adff5bd895f9375f681bab4c04007cc0f4a17792
Pavan Bokka <rampavan.bokka@gmail.com>
2025-08-06 10:43:30 +0530

    new commands added

---
+++ functions.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "Error: please run this script with root access"
+    exit1
+else
+    echo "You are running with root access"
+fi     
+VALIDATE(){
+   if [ $1 -eq 0 ]
+    then
+        echo "Installing MySQL is SUCCESS."
+    else
+        echo "Installing MySQL is FAILURE."
+        exit 1
+    fi  
+}
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed...initialising installation"
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else
+    echo "MySQL is already installed."
+fi
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed...initialising installation"
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo "python3 is already installed."
+fi
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    echo "Nginx is not installed...initialising installation"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo "nginx is already installed."
+fi
