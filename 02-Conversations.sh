512b44eaf84c1b858601ddbde121883599467651
Pavan Bokka <rampavan.bokka@gmail.com>
2025-08-07 11:17:44 +0530

    file renamed

---
+++ 02-Conversations.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Trump: Hey Musk, How are you?"
+echo "Musk: Hello Trump, I am fine. How you doing?"
+echo "Trump: I am fine too. What's up?"
+echo "Musk: Nothing much.Going to mars now, will you come?"
