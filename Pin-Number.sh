404acdb8c69e3b2618d750ec88065cbd9e08df77
Pavan Bokka <rampavan.bokka@gmail.com>
2025-08-07 11:40:15 +0530

    file renamed

--- Pin-Number.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "enter your pin::"
-read -s PIN #here PIN is variable
-echo "your number is: $PIN"
--- Time-Stamp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-TIMESTAMP=$(date)
-echo "Script executed at: $TIMESTAMP"
