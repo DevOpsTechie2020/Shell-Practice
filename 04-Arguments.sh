e88e07338df92d9937751728937b57bbda82ee02
Pavan Bokka <rampavan.bokka@gmail.com>
2025-08-07 11:29:23 +0530

    file renamed

---
+++ 04-Arguments.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1 #there should not be space between lable and key
+PERSON2=$2
+echo "$PERSON1: Hey $PERSON2, How are you?"
+echo "$PERSON2: Hello $PERSON1, I am fine. How you doing?"
+echo "$PERSON1: I am fine too. What's up?"
+echo "$PERSON2: Nothing much.Going to mars now, will you come?"
